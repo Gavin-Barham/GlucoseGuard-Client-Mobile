575cf0b5d5fe7e827c2d30c45592815aa002bd55
Gavin Barham <gavinbarham@yahoo.com>
2024-02-23 12:37:53 -0600

    Imported tsQuery and wrapped app in queryProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,19 @@
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-// Screens
-import Login from './Screens/Login'
-import Register from './Screens/Register';
-import Home from './Screens/Home';
-
-type RootStackParamList = {
-  Register: undefined;
-  Login: undefined;
-  Home: undefined;
-};
-
-const Stack = createNativeStackNavigator<RootStackParamList>();
-
-const MyStack = () => (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Register" component={Register} />
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
-);
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { UserProvider } from './Context/UserContext';
+import Stacks from './Stacks';
 
 
 export default function App() {
+    const queryClient = new QueryClient();
     return (
-        <>
-            {MyStack()}
-        </>
+      <QueryClientProvider client={queryClient}>
+		    <UserProvider >
+            <Stacks />
+		    </UserProvider>
+      </QueryClientProvider>
     );
+    
 }
