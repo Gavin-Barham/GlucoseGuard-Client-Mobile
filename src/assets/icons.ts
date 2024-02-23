7f28c94437c79ce25771fd8c420787e7cbe15fb6
Gavin Barham <gavinbarham@yahoo.com>
2024-02-23 12:36:20 -0600

    added font awesome icons assets

---
+++ src/assets/icons.ts
@@ -0,0 +1,15 @@
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faHouse } from '@fortawesome/free-solid-svg-icons/faHouse';
+import { faStaffSnake } from '@fortawesome/free-solid-svg-icons/faStaffSnake';
+import { faBurger } from '@fortawesome/free-solid-svg-icons/faBurger';
+import { faDumbbell } from '@fortawesome/free-solid-svg-icons/faDumbbell';
+import { faGear } from '@fortawesome/free-solid-svg-icons/faGear';
+
+export {
+    FontAwesomeIcon,
+    faHouse,
+    faStaffSnake,
+    faBurger,
+    faDumbbell,
+    faGear
+}
