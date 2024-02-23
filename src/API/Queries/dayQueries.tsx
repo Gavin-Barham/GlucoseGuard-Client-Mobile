2f0f42442f9154383d227bf55c21ea0c85a47495
Gavin Barham <gavinbarham@yahoo.com>
2024-02-23 13:27:54 -0600

    removed mutation and added createDay call to proceed getDay since server will only create if not exists

--- src/API/Queries/dayQueries.tsx
+++ src/API/Queries/dayQueries.tsx
@@ -1,22 +1,14 @@
 import { useQuery } from "@tanstack/react-query"
-import { getDay } from "../requests"
+import { createDay, getDay } from "../requests"
 import { User } from "Types/users"
 
 export const useDayQuery = (user: User, date: string) => {
     return (useQuery({
             queryKey: ["day"],
-            queryFn: () => getDay(user, date),
+            queryFn: () => {
+                createDay(user, date);
+                return getDay(user, date)
+            },
         })
     );
 };
-// export const useDayMutation = (user: User, date: string) => {
-//     const queryClient = useQueryClient();
-//     return (useMutation({
-//             mutationFn: async () => await createDay(user, date),
-//             onSuccess: async () => await queryClient.invalidateQueries({
-//                 queryKey: ['day'],
-//                 exact: true,
-//             })
-//         })
-//     );
-// };
